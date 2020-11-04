864cdf1d3da667b37b2524caba8a59bad237f775
jinny20123 <jinny20123@gmail.com>
2020-11-05 01:38:10 +0900

    키움증권 계좌 연동해서 보유 종목 가져오기

--- connect_kiwoom/ConnectKiwoom.py
+++ connect_kiwoom/ConnectKiwoom.py
@@ -1,6 +1,9 @@
+#-*-coding:utf-8 -*-
+
 import sys
 import csv
 
+import json
 from PyQt5.QAxContainer import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -10,14 +13,14 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Plutus')
-        self.setGeometry(300, 300, 300, 400)
+        self.setGeometry(200, 200, 600, 800)
 
         # 키움 OpenAPI 라이브러리 추가 
         self.kiwoom = QAxWidget('KHOPENAPI.KHOpenAPICtrl.1')
         self.kiwoom.dynamicCall('CommConnect()')
 
         self.text_edit = QTextEdit(self)
-        self.text_edit.setGeometry(10, 260, 280, 100)
+        self.text_edit.setGeometry(10, 260, 560, 200)
         self.text_edit.setEnabled(False)
 
         # 코드 검색용 UI 인터페이스
@@ -38,12 +41,18 @@ class MyWindow(QMainWindow):
         getCodebtn.move(190, 60)
         getCodebtn.clicked.connect(self.getCodebtn_clicked)
 
+        # 버튼 
+        getAccountbtn = QPushButton('계좌 정보 조회', self)
+        getAccountbtn.move(190, 100)
+        getAccountbtn.clicked.connect(self.getAccountbtn_clicked)
+
         self.listWidget = QListWidget(self)
         self.listWidget.setGeometry(10, 60, 170, 130)
 
         # 콜백이벤트 등록
         self.kiwoom.OnEventConnect.connect(self.on_connect)
         self.kiwoom.OnReceiveTrData.connect(self.on_receiveTr)
+
 
     # 검색용 버튼 클릭 이벤트 함수
     def findbtn_clicked(self):
@@ -54,31 +63,39 @@ class MyWindow(QMainWindow):
         self.kiwoom.dynamicCall('SetInputValue(QString, QString)', '종목코드', code)
 
         # CommRqData
-        self.kiwoom.dynamicCall('CommRqData(QString, QString, int, QString)', 'opt10001_req', 'opt10001', 0, '0101')
+        self.kiwoom.dynamicCall('CommRqData(QString, QString, int, QString)', '주식기본정보조회', 'opt10001', 0, '10001')
 
-    # 마켓 정보 가져오기
+    def getAccountbtn_clicked(self):
+        self.kiwoom.dynamicCall('SetInputValue(QString, QString)', '계좌번호', self.account_num.rstrip(';'))
+        self.kiwoom.dynamicCall('CommRqData(QString, QString, int, QString)', '실시간잔고', 'opt10085', 0, '10002')
+
+    # 마켓 정보 가져오기 버튼
     def getCodebtn_clicked(self):
-        stockInfoList = self.get_MarketList('kospi')
+        stockInfoList = self.get_StockListInfo(self.kospi_market_list)
         self.text_edit.append('kospi MarkerCode Count = ' +  str(len(stockInfoList)))
 
+        # to csv 파일
         with open('C:/Users/Kim/Documents/Plutus/DataBase/StockInfo.csv','w+' , encoding='utf-8', newline='') as f :            
             writer = csv.writer(f)
             for stockInfo in stockInfoList:
                 writer.writerow(stockInfo)
 
+    
+
+    # 마켓 정보리스트 가져오기
     def get_MarketList(self, marketType):
         callRet = ''
-        stockInfoList =[]
         if marketType == 'kospi':
             callRet = self.kiwoom.dynamicCall('GetCodeListByMarket(QString)', ['0'])
         elif marketType == 'kosdaq':
             callRet = self.kiwoom.dynamicCall('GetCodeListByMarket(QString)', ['10'])
-        else :
-            return stockInfoList
 
         code_list = callRet.split(';')
+        return code_list;
 
-        for x in code_list :
+    def get_StockListInfo(self, stock_list):
+        stockInfoList =[]
+        for x in stock_list :
             name = self.kiwoom.dynamicCall('GetMasterCodeName(QString)', [x])
             stockInfoList.append([x,name])
         return stockInfoList
@@ -88,16 +105,34 @@ class MyWindow(QMainWindow):
     def on_connect(self, err_code):
         if err_code == 0:
             self.text_edit.append('Login On')
+            self.kospi_market_list = self.get_MarketList('kospi')
+            self.kosdaq_market_list = self.get_MarketList('kosdaq')
+            self.account_num = self.kiwoom.dynamicCall("GetLoginInfo(QString)", ["ACCNO"])
+            self.text_edit.append("계좌번호: " + self.account_num.rstrip(';'))
         else: 
             self.text_edit.append('Login Fail err =' +  err_code)
 
     # Tr 이벤트 콜백 함수
     def on_receiveTr(self, screen_no, rqname, trcode, recordname, prev_next, data_len, err_code, msg1, msg2):
-        if rqname == 'opt10001_req':
-            name = self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, 0, '종목명')
-            volume = self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, 0, '거래량')
-            self.text_edit.append('종목명: ' + name.strip())
-            self.text_edit.append('거래량: ' + volume.strip())
+        # 종목 정보 조회
+        if rqname == '주식기본정보조회':
+            stock_data = {}
+            stock_data['name'] = self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, 0, '종목명').strip()
+            stock_data['volume'] = self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, 0, '거래량').strip()
+            stock_data['ROE'] = self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, 0, 'ROE').strip()
+            stock_data_json = json.dumps(stock_data, ensure_ascii=False)
+            self.text_edit.append('종목정보: ' + stock_data_json)
+        elif rqname == '실시간잔고':
+            stock_dataList = []
+            data_count = self.kiwoom.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
+            for i in range(data_count):
+                stock_data = {}
+                stock_data['name']= self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, i, '종목명').strip()
+                stock_data['buy_price']= self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, i, '매입금액').strip()
+                stock_data['cur_price']= self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString)', trcode, '', rqname, i, '평가금액').strip()
+                stock_dataList.append(stock_data)
+            stock_data_json = json.dumps(stock_dataList, ensure_ascii=False)
+            self.text_edit.append('계좌평가현황: ' + stock_data_json)
 
 if __name__ == '__main__' :
     app = QApplication(sys.argv)
